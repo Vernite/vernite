db596791557ac57d3615fa15a27c4f5bbdc81ba5
Marcin Czerniak <marcin.czerniak@idevelopment.pl>
2022-04-12 10:32:28 +0200

    chore: Architecture reorganization with action co deploy documentation

---
+++ src/app/_common/_common.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-_common',
+  templateUrl: './_common.component.html',
+  styleUrls: ['./_common.component.scss'],
+})
+export class _commonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/_common/_common.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { _commonComponent } from './_common.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [_commonComponent],
+})
+export class _commonModule {}
