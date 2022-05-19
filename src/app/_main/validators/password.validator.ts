a28f2a40678f8b0c987d3a02dd5a3a2a7529000b
Alexia <74705079+TheDarkestQueen@users.noreply.github.com>
2022-05-19 23:23:55 +0200

    fix

--- src/app/_main/validators/password.validator.ts
+++ src/app/_main/validators/password.validator.ts
@@ -7,7 +7,7 @@ import { ValidationError } from '../interfaces/validation-error.interface';
  */
 export function passwordValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationError | null => {
-    if (control.value && !/^(?=.*[A-Za-z])(?=.*\d)[.]{8,}$/.test(control.value)) {
+    if (control.value && !/^(?=.*[A-Za-z])(?=.*\d).{8,}$/.test(control.value)) {
       return {
         type: 'password',
         message: $localize`Password requires minimum eight characters, at least one letter and one number`,
