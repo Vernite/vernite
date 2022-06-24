da4759606b9ac234793cbf4d1432bb72239cf221
Marcin Czerniak <marc999.mc@gmail.com>
2022-06-24 11:50:43 +0200

    chore: Task type indicators and change status overlay styling

--- src/app/tasks/pipes/task-type.pipe.ts
+++ src/app/tasks/pipes/task-type.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { TaskType } from '@tasks/enums/task-type.enum';
+import { SubTaskType, TaskType } from '@tasks/enums/task-type.enum';
 import * as Color from 'color';
 
 @Pipe({
@@ -10,11 +10,9 @@ export class TaskTypePipe implements PipeTransform {
   transform(value: any, type: 'color'): Color;
 
   transform(value: any, type: 'color' | 'name' | 'icon' = 'name'): string | Color {
-    if (typeof value === 'number') value = TaskType[value];
-
-    switch (value) {
+    switch (value as TaskType | SubTaskType) {
       // Task
-      case 'TASK':
+      case TaskType.TASK:
         return {
           name: $localize`Task`,
           color: Color.rgb(52, 152, 219),
@@ -22,7 +20,7 @@ export class TaskTypePipe implements PipeTransform {
         }[type];
 
       // User story
-      case 'USER_STORY':
+      case TaskType.USER_STORY:
         return {
           name: $localize`User story`,
           color: Color.rgb(46, 204, 113),
@@ -30,7 +28,7 @@ export class TaskTypePipe implements PipeTransform {
         }[type];
 
       // Issue
-      case 'ISSUE':
+      case TaskType.ISSUE:
         return {
           name: $localize`Issue`,
           color: Color.rgb(231, 76, 60),
@@ -38,7 +36,7 @@ export class TaskTypePipe implements PipeTransform {
         }[type];
 
       // Epic
-      case 'EPIC':
+      case TaskType.EPIC:
         return {
           name: $localize`Epic`,
           color: Color.rgb(155, 89, 182),
@@ -46,7 +44,7 @@ export class TaskTypePipe implements PipeTransform {
         }[type];
 
       // Subtask
-      case 'SUBTASK':
+      case SubTaskType.SUBTASK:
         return {
           name: $localize`Subtask`,
           color: Color.rgb(88, 183, 189),
