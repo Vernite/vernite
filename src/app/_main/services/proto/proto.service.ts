0c8459281fb8fb89bf6dc4623cc299ce05d8f142
Marcin Czerniak <marc999.mc@gmail.com>
2022-11-23 10:52:39 +0100

    Proto serialization and deserialization

--- src/app/_main/services/proto/proto.service.ts
+++ src/app/_main/services/proto/proto.service.ts
@@ -1,23 +1,76 @@
 import { Injectable } from '@angular/core';
-import { Service } from '@main/decorators/service/service.decorator';
-import { NewTaskNotification } from '@vernite/protobuf/notifications/task.notification_pb';
+import { NewTaskNotification } from '@vernite/protobuf/notifications/task.notification.js';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { filter, from, map, of } from 'rxjs';
+import { Message } from 'google-protobuf';
+import { Any } from 'google-protobuf/google/protobuf/any_pb.js';
 
-@Service()
 @Injectable({
   providedIn: 'root',
 })
 export class ProtoService {
-  constructor() {
-    const webSocket = new WebSocket('wss://vernite.dev/api/ws');
+  private _websocket$!: WebSocketSubject<any>;
 
-    const message = this.next();
-    console.log(message);
+  // public TASKS = this._websocket$.pipe(this.isType(NewTaskNotification));
+
+  constructor() {
+    this.connect();
   }
 
-  public next() {
-    const notification = new NewTaskNotification();
-    notification.setName('test');
-    notification.setCreatedat('test');
-    return notification;
+  connect() {
+    this._websocket$ = webSocket({
+      url: 'wss://vernite.dev/api/ws',
+      deserializer: (e: MessageEvent) => e,
+      serializer: (value: any) => value,
+    });
+
+    this._websocket$.subscribe((message: MessageEvent) => {
+      this.deserialize(message).subscribe((message: Message) => {
+        console.log(message);
+      });
+    });
+
+    console.log(
+      new NewTaskNotification({
+        name: 'Test',
+        createdAt: '2021-01-01',
+        taskId: 1,
+      }).serializeBinary(),
+    );
+
+    this.serialize(
+      new NewTaskNotification({
+        name: 'Test',
+        createdAt: '2021-01-01',
+        taskId: 1,
+      }),
+    ).subscribe((data) => {
+      this._websocket$.next(data);
+    });
+  }
+
+  isType(cls: typeof Message) {
+    return filter((message: Message) => {
+      return message.getJsPbMessageId() === cls.name;
+    });
+  }
+
+  serialize(value: Message) {
+    const any = new Any();
+    any.pack(value.serializeBinary(), value.constructor.name);
+    return of(any.serializeBinary());
+  }
+
+  deserialize(e: MessageEvent) {
+    return from(e.data.arrayBuffer() as Promise<Uint8Array>).pipe(
+      map((buffer: Uint8Array) => {
+        const any = Any.deserializeBinary(buffer);
+        const type = any.getTypeName();
+        const message = any.unpack((bytes: Uint8Array) => {
+          return any.getValue().deserializeBinary(bytes);
+        }, type);
+        return message!;
+      }),
+    );
   }
 }
