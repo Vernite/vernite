cc4e5e054743baed74c1f3578c272865c278ca3a
Marcin Czerniak <marc999.mc@gmail.com>
2022-11-25 11:12:10 +0100

    fix: Fix import statement

--- src/app/dashboard/modules/integration-modules/decorators/integration-module/integration-module.decorator.ts
+++ src/app/dashboard/modules/integration-modules/decorators/integration-module/integration-module.decorator.ts
@@ -1,7 +1,7 @@
 import { IntegrationModuleEntryConfig } from '@main/interfaces/integration-module.interface';
 import { uniqueId } from 'lodash-es';
-import { IntegrationComponent } from '../../../../../../../dist/out-tsc/src/app/dashboard/modules/integration-modules/interfaces/integration-component.interface';
 import { Type } from '@angular/core';
+import { IntegrationComponent } from '../../interfaces/integration-component.interface';
 
 export function IntegrationModule(config: IntegrationModuleEntryConfig) {
   return function decorator(target: Type<IntegrationComponent>) {
