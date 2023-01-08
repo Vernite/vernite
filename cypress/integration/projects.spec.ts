63a2def6e3e446afd841d253dc98ad75f97802be
Alexia <74705079+TheDarkestQueen@users.noreply.github.com>
2023-01-08 18:30:49 +0100

    fix e2e tests

--- cypress/integration/projects.spec.ts
+++ cypress/integration/projects.spec.ts
@@ -1,6 +1,8 @@
 import auth from '../fixtures/auth.json';
 
 describe('Projects tests', () => {
+  const rand = Math.random();
+
   before(() => {
     cy.logout();
     cy.login(auth.email, auth.password);
@@ -13,9 +15,9 @@ describe('Projects tests', () => {
   });
 
   it('Should be able to create a new project', () => {
-    cy.contains('td', 'test workspace').click();
+    cy.contains('td', 'test workspace ').click();
     cy.contains('New project').click();
-    cy.get('input[name=name]').type('Test project');
+    cy.get('input[name=name]').type('Test project ' + rand);
     cy.contains('button', 'Create project').click();
   });
 
@@ -29,18 +31,24 @@ describe('Projects tests', () => {
 
   it('Should be able to edit a project', () => {
     cy.contains('td', 'test workspace').click();
-    cy.contains('tr', 'Test project').find('.mat-menu-trigger').click();
+    cy.contains('tr', 'Test project ' + rand)
+      .find('.mat-menu-trigger')
+      .click();
     cy.contains('Edit').click();
-    cy.get('input[name=name]').clear().type('Test project - renamed');
+    cy.get('input[name=name]')
+      .clear()
+      .type('Test project - renamed ' + rand);
     cy.contains('button', 'Save changes').click();
-    cy.contains('Test project - renamed');
+    cy.contains('Test project - renamed ' + rand);
   });
 
   it('Should be able to delete a project', () => {
     cy.contains('td', 'test workspace').click();
-    cy.contains('tr', 'Test project - renamed').find('.mat-menu-trigger').click();
+    cy.contains('tr', 'Test project - renamed ' + rand)
+      .find('.mat-menu-trigger')
+      .click();
     cy.contains('Delete').click();
     cy.get('.mat-dialog-container').contains('button', 'Delete').click();
-    cy.contains('Test project - renamed').should('not.exist');
+    cy.contains('Test project - renamed ' + rand).should('not.exist');
   });
 });
--- cypress/integration/workspaces.spec.ts
+++ cypress/integration/workspaces.spec.ts
@@ -1,6 +1,8 @@
 import auth from '../fixtures/auth.json';
 
 describe('Workspaces tests', () => {
+  const rand = Math.random();
+
   beforeEach(() => {
     cy.logout();
     cy.login(auth.email, auth.password);
@@ -9,9 +11,9 @@ describe('Workspaces tests', () => {
 
   it('Should be able to create a new workspace', () => {
     cy.contains('New workspace').click();
-    cy.get('input[name=name]').type('Test workspace');
+    cy.get('input[name=name]').type('Test workspace ' + rand);
     cy.contains('button[type=submit]', 'Save').dblclick();
-    cy.contains('Test workspace');
+    cy.contains('Test workspace ' + rand);
   });
 
   it('Should not be able to create a new workspace without name', () => {
@@ -21,17 +23,23 @@ describe('Workspaces tests', () => {
   });
 
   it('Should be able to edit a workspace', () => {
-    cy.contains('tr', 'Test workspace').find('.mat-menu-trigger').click();
+    cy.contains('tr', 'Test workspace ' + rand)
+      .find('.mat-menu-trigger')
+      .click();
     cy.contains('Edit').click();
-    cy.get('input[name=name]').clear().type('Test workspace - renamed');
+    cy.get('input[name=name]')
+      .clear()
+      .type('Test workspace - renamed ' + rand);
     cy.contains('button[type=submit]', 'Save').click();
-    cy.contains('Test workspace - renamed');
+    cy.contains('Test workspace - renamed ' + rand);
   });
 
   it('Should be able to delete a workspace', () => {
-    cy.contains('tr', 'Test workspace - renamed').find('.mat-menu-trigger').click();
+    cy.contains('tr', 'Test workspace - renamed ' + rand)
+      .find('.mat-menu-trigger')
+      .click();
     cy.contains('Delete').click();
     cy.get('.mat-dialog-container').contains('button', 'Delete').click();
-    cy.contains('Test workspace - renamed').should('not.exist');
+    cy.contains('Test workspace - renamed ' + rand).should('not.exist');
   });
 });
