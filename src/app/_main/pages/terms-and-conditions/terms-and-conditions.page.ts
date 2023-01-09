5ea0493db0ac397a564d63ab8461b2f850b9e735
Marcin Czerniak <marc999.mc@gmail.com>
2023-01-09 22:35:20 +0100

    Fix privacy policy and terms and consitions router links

--- src/app/_main/pages/terms-and-conditions/terms-and-conditions.page.ts
+++ src/app/_main/pages/terms-and-conditions/terms-and-conditions.page.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-terms-and-conditions',
   templateUrl: './terms-and-conditions.page.html',
   styleUrls: ['./terms-and-conditions.page.scss'],
 })
-export class TermsAndConditionsPage {}
+export class TermsAndConditionsPage implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const tree = this.router.parseUrl(this.router.url);
+        if (tree.fragment) {
+          const element = document.querySelector('#' + tree.fragment);
+          if (element) {
+            element.scrollIntoView();
+          }
+        }
+      }
+    });
+  }
+}
