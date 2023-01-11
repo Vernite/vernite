d8f1de75aae2e31890c4c7110a4af3b87321a8ad
Alexia <74705079+TheDarkestQueen@users.noreply.github.com>
2023-01-11 02:07:38 +0100

    fix of the fix ;)

--- src/app/auth/services/user/user.service.spec.ts
+++ src/app/auth/services/user/user.service.spec.ts
@@ -4,12 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { inject, TestBed } from '@angular/core/testing';
 import { UserService } from './user.service';
 import { RECAPTCHA_V3_SITE_KEY, ReCaptchaV3Service } from 'ng-recaptcha';
+import { MatDialogModule } from '@angular/material/dialog';
 import { environment } from '../../../../environments/environment';
 
 describe('Service: User', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientModule, MatDialogModule],
       providers: [
         UserService,
         { provide: RECAPTCHA_V3_SITE_KEY, useValue: environment.captchaSiteKey },
