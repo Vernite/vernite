91542390b58e76f4e89c5ad3e71c49367265172b
Marcin Czerniak <marc999.mc@gmail.com>
2023-01-30 20:01:19 +0100

    Feature/configs (#2)
    * wip: Configs architecture
    
    * Remove useless .angulardoc.json file

--- packages/frontend/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  trailingComma: "all",
-  tabWidth: 2,
-  semi: true,
-  singleQuote: true,
-  bracketSpacing: true,
-  printWidth: 100,
-  htmlWhitespaceSensitivity: "strict",
-};
