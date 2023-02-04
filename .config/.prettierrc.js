c16b5a7587c1165af4b3cc1a13a9c115e99b395c
Marcin Czerniak <marc999.mc@gmail.com>
2023-02-04 15:31:40 +0100

    Frontend proxy

---
+++ .config/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  printWidth: 100,
+  htmlWhitespaceSensitivity: "strict",
+};
