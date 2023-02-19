70d8f3abcdd12070499ab80f5d5d083c5b8c119c
Marcin Czerniak <marc999.mc@gmail.com>
2023-02-19 18:59:19 +0100

    Summary generation testing

--- .github/actions/summary/index.js
+++ .github/actions/summary/index.js
@@ -2,3 +2,17 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 core.notice('This is notice');
+
+(async () => {
+  await core.summary
+    .addHeading('Test Results')
+    .addCodeBlock(generateTestResults(), "js")
+    .addTable([
+      [{ data: 'File', header: true }, { data: 'Result', header: true }],
+      ['foo.js', 'Pass '],
+      ['bar.js', 'Fail '],
+      ['test.js', 'Pass ']
+    ])
+    .addLink('View staging deployment!', 'https://github.com')
+    .write();
+})();
