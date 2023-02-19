e8e850c5d01d41d94e4518ba487cd378f1a84e80
Marcin Czerniak <marc999.mc@gmail.com>
2023-02-19 19:08:06 +0100

    Summary generation testing

--- .github/actions/summary/index.js
+++ .github/actions/summary/index.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const successMark = '<svg width="14" height="14" class="octicon octicon-check-circle-fill color-fg-success" aria-label="completed successfully" viewBox="0 0 16 16" version="1.1" role="img"><path fill="#3FB950" fill-rule="evenodd" d="M8 16A8 8 0 108 0a8 8 0 000 16zm3.78-9.72a.75.75 0 00-1.06-1.06L6.75 9.19 5.28 7.72a.75.75 0 00-1.06 1.06l2 2a.75.75 0 001.06 0l4.5-4.5z"></path></svg>';
+
 core.notice('This is notice');
 
 (async () => {
@@ -9,7 +11,7 @@ core.notice('This is notice');
     .addCodeBlock('console.log("sample code block")', "js")
     .addTable([
       [{ data: 'File', header: true }, { data: 'Result', header: true }],
-      ['foo.js', 'Pass '],
+      [successMark, 'Pass '],
       ['bar.js', 'Fail '],
       ['test.js', 'Pass ']
     ])
